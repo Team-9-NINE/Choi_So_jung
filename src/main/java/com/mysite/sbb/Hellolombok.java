46fbcb254a8235c5032d1537f7bdf35b7b2e9da9
Team-9-NINE <choisojung0319@daum.net>
2025-11-06 22:40:31 +0900

    H2 DB 연결 및 JPA Repository Test

--- src/main/java/com/mysite/sbb/Hellolombok.java
+++ src/main/java/com/mysite/sbb/Hellolombok.java
@@ -1,18 +1,17 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.RequiredArgConstructor;
-@RequiredArgsConstructorimport lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public class HelloLombok {
-	private final String hello;
-	private final int lombok;
-	
-	public static void main(String[] args) {
-		HelloLombok helloLombok = new HelloLombok("헬로", 5);
-		System.out.println(helloLombok.getHello());
-		System.out.println(helloLombok.getLombok());
-	}
+    private final String hello;
+    private final int lombok;
+    
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
 }
