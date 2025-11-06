e7479ef28b885adf4367a82d3114a89be3355151
Team-9-NINE <choisojung0319@daum.net>
2025-11-06 23:55:15 +0900

    URL 프리픽스 알아두기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/question")
 
 @RequiredArgsConstructor
 
@@ -16,7 +19,7 @@ public class QuestionController {
 	
 	private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
    
     public String list(Model model) {
     	List<Question> questionList = this.questionService.getList();
@@ -24,7 +27,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
     	Question question = this.questionService.getQuestion(id);
     	model.addAttribute("question", question);
